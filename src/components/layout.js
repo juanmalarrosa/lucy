ced6c17cd07e76d221bf5ba935a36956801650cc
juanmalarrosa <juanmanuellarrosa@gmail.com>
2021-09-04 20:23:11 -0300

    hero

--- src/components/layout.js
+++ src/components/layout.js
@@ -2,34 +2,63 @@ import * as React from "react"
 import { Link } from "gatsby"
 
 const Layout = ({ location, title, children }) => {
-  const rootPath = `${__PATH_PREFIX__}/`
-  const isRootPath = location.pathname === rootPath
-  let header
-
-  if (isRootPath) {
-    header = (
-      <h1 className="main-heading">
-        <Link to="/">{title}</Link>
-      </h1>
-    )
-  } else {
-    header = (
-      <Link className="header-link-home" to="/">
-        {title}
-      </Link>
-    )
-  }
-
+ 
   return (
-    <div className="global-wrapper" data-is-root-path={isRootPath}>
-      <header className="global-header">{header}</header>
+    <>
+      {/* HEADER */}
+       <header className="c-header is-open" id="c-header">
+        <div className="c-header__holder">
+          <div className="c-header__wrapper o-wrapper o-align-middle">{/*
+                    */}<div className="o-align-middle__aligner">
+              <button type="button" className="c-header__nav-btn js-show-nav is-active" title="Ver/ocultar menú"><span className="c-header__hamburger" /></button> 
+              <h1 className="c-header__h1">
+                <a href="/" className="c-header__logo" tabIndex={-1}>
+                  <span className="u-hidden-for-seo">Lucy</span>
+                </a>
+              </h1>
+              <div className="c-header__hidden">
+                <div className="c-header__viewport js-nav" style={{display: 'none'}}>
+                  <div className="c-header__nav o-scrollable">{/*
+                                    */}<nav className="c-nav">
+                      <h2 className="u-hidden-for-seo">Navegación</h2>
+                      <ul className="c-nav__items">
+                        <li className="c-nav__item">
+                          <a className="c-nav__link js-nav__anchor" href="#benefits">Por qué usar Lucy?</a>
+                        </li>
+                        <li className="c-nav__item">
+                          <a className="c-nav__link js-nav__anchor" href="#software">Cómo funciona?</a>
+                        </li>
+                      </ul>
+                      <ul className="c-nav__buttons">
+                        <li className="c-nav__item-btn">
+                          <button className="c-nav__btn c-nav__btn--admin c-admin-btn-desktop js-show-admin c-btn" type="button">
+                            Quiero registrarme!
+                          </button>
+                        </li>
+                      </ul>
+                    </nav>{/* .c-nav */}
+                  </div>{/* .c-header__nav */}
+                </div>
+              </div>
+              <div className="c-viewport__overlay" style={{display: 'none'}} />
+            </div>{/* .o-align-middle__aligner */}
+          </div>{/* .c-header__wrapper */}
+        </div>{/* .c-header__holder */}
+      </header>
+
+
       <main>{children}</main>
-      <footer>
-        © {new Date().getFullYear()}, Built with
-        {` `}
-        <a href="https://www.gatsbyjs.com">Gatsby</a>
+
+
+      {/* FOOTER */}
+      <footer className="c-footer">
+        <div className="o-wrapper">
+          Lucy
+        </div>
       </footer>
-    </div>
+
+
+    </>
   )
 }
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -5,17 +5,102 @@ import Seo from "../components/seo"
 
 const Index = ({ data, location }) => {
 
-    return (
-      <Layout location={location} title={'h'}>
-        <Seo title="All posts" />
-        <p>
-          No blog posts found. Add markdown posts to "content/blog" (or the
-          directory you specified for the "gatsby-source-filesystem" plugin in
-          gatsby-config.js).
-        </p>
-      </Layout>
-    )
-  
+  return (
+
+    <Layout location={location} title={'h'}>
+      <Seo title="Lucy" />
+
+
+      {/* HERO */}
+      <section className="c-hero">
+        <div className="o-wrapper c-hero__wrapper">
+          <h2 className="c-hero-title">
+            Tomá mejores decisiones‏‏‎ ‎‏‏‎<br/>
+          <span className="c-hero-title__mark">y generá más‎‎‎‎‎‎‎‎‎‎‎‎ rentabilidad</span>
+          </h2>
+          <p className="c-hero__text">
+            Lucy es la plataforma que te permite recibir recomendaciones de inversión <strong>precisas</strong> y en el <strong>momento justo</strong>.
+          </p>
+
+          <div className="c-cta">
+            <p className="c-cta__title">Registrate para el pre-lanzamiento y <strong>obtené 6 meses de recomendaciones gratis</strong></p>
+            <form className="c-cta__form">
+              <input className="c-cta__input" placeholder="Ingresa tu email..." />
+              <button type="submit" className="c-cta__btn"></button>
+            </form>
+          </div>
+        </div>
+      </section>
+
+      {/* BENEFITS */}
+      <section className="c-benefits o-section">
+        <div className="o-wrapper">
+          <h2 className="c-title">Por qué usar Lucy?</h2>
+          <ul className="c-benefits-list">
+
+            <li className="c-benefits-item">
+              <div className="c-benefits-item__holder">
+                <span className="c-benefits-item__icon"></span>
+                <h3 className="c-benefits-item__title">Es más preciso</h3>
+                <p className="c-benefits-item__text">Comprar y vender acciones
+es fácil, lo difícil es saber
+cuándo. Lucy te ayuda a
+generar rentabilidad con
+pasos firmes y seguros.</p>
+              </div>
+            </li>
+            <li className="c-benefits-item">
+              <div className="c-benefits-item__holder">
+                <span className="c-benefits-item__icon"></span>
+                <h3 className="c-benefits-item__title">Es más preciso</h3>
+                <p className="c-benefits-item__text">Comprar y vender acciones
+es fácil, lo difícil es saber
+cuándo. Lucy te ayuda a
+generar rentabilidad con
+pasos firmes y seguros.</p>
+              </div>
+            </li>
+            <li className="c-benefits-item">
+              <div className="c-benefits-item__holder">
+                <span className="c-benefits-item__icon"></span>
+                <h3 className="c-benefits-item__title">Es más preciso</h3>
+                <p className="c-benefits-item__text">Comprar y vender acciones
+es fácil, lo difícil es saber
+cuándo. Lucy te ayuda a
+generar rentabilidad con
+pasos firmes y seguros.</p>
+              </div>
+            </li>
+
+          </ul>
+        </div>
+
+      </section>
+
+
+
+      {/* HOW IT WORKS */}
+      <section className="c-works o-section">
+        <div className="o-wrapper">
+          <h2 className="c-title">Cómo funciona?</h2>
+
+        </div>
+      </section>
+
+
+      {/* CTA */}
+      <div className="c-cta">
+        <p className="c-cta__title">Registrate para el pre-lanzamiento y obtené 6 meses de recomendaciones gratis</p>
+        <form className="c-cta__form">
+          <input className="c-cta__input" />
+          <button type="submit" className="c-cta__btn"></button>
+        </form>
+      </div>
+
+
+    </Layout>
+  )
+
 }
 
 export default Index

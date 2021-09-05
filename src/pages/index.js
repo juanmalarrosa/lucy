73f35a5abb6289bf7ff46b8bf9e3af78faf8a2d6
juanmalarrosa <juanmanuellarrosa@gmail.com>
2021-09-05 16:49:05 -0300

    form

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,18 +10,19 @@ import {
 } from 'formik'
 
 
-
 const Index = ({ data, location }) => {
 
   const encode = (data) => {
     return Object.keys(data)
-          .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
-          .join("&");
-}
+      .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+      .join("&");
+  }
+
+  const [message, setMessage] = React.useState(false)
 
   return (
 
-    <Layout location={location} title={'h'}>
+    <Layout location={location}>
       <Seo title="Lucy | Generá mas rentabilidad" />
 
 
@@ -37,7 +38,7 @@ const Index = ({ data, location }) => {
           </p>
 
           <div className="c-cta">
-            <p className="c-cta__title">Registrate para el pre-lanzamiento y <strong>obtené <br /> 6 meses de recomendaciones gratis</strong></p>
+            <p className="c-cta__title">Registrate para el pre-lanzamiento y <strong>obtené 6 meses de recomendaciones gratis</strong></p>
 
 
 
@@ -55,6 +56,7 @@ const Index = ({ data, location }) => {
                   })
                     .then(() => {
 
+                      setMessage(true)
                       actions.resetForm()
 
 
@@ -83,19 +85,25 @@ const Index = ({ data, location }) => {
               {() => (
                 <Form key="cta" name="cta" method="POST" data-netlify="true" netlify-honeypot="bot-field" className="c-cta__form">
 
-                  <p className="u-hidden-for-seo">
-                    <label>Don’t fill this out if you’re human: <input name="bot-field" /></label>
-                  </p>
+                  {!message &&
+                    <>
+                      <p className="u-hidden-for-seo">
+                        <label>Don’t fill this out if you’re human: <input name="bot-field" /></label>
+                      </p>
 
-                  <input type="hidden" name="form-name" value="newsletter" />
+                      <input type="hidden" name="form-name" value="newsletter" />
 
-                  <Field name="email" type="email" className="c-cta__input" placeholder="Ingresá tu email..." />
+                      <Field name="email" type="email" className="c-cta__input" placeholder="Ingresá tu email..." />
 
-                  <label for="email" className="c-cta__error"><ErrorMessage name="email" /></label>
+                      <label for="email" className="c-cta__error"><ErrorMessage name="email" /></label>
 
-                  <div data-netlify-recaptcha="true"></div>
+                      <div data-netlify-recaptcha="true"></div>
 
-                  <button type="submit" className="c-cta__btn">Enviar</button>
+                      <button type="submit" className="c-cta__btn">Enviar</button>
+                    </>
+                  }
+
+                  {message && <p className="c-cta__message"><strong>Gracias por registrarte!</strong> <br /> Nos comunicaremos a la brevedad.</p>}
 
                 </Form>
               )}
@@ -174,10 +182,77 @@ para hacerlo crecer.</p>
       <div id="cta" className="c-cta c-cta--footer">
         <div className="o-wrapper">
           <p className="c-cta__title">Registrate para el pre-lanzamiento y <strong>obtené 6 meses de recomendaciones gratis</strong></p>
-          <form className="c-cta__form">
-            <input className="c-cta__input" placeholder="Ingresá tu email..." />
-            <button type="submit" className="c-cta__btn">Enviar</button>
-          </form>
+
+
+          <Formik
+            initialValues={{
+              email: ''
+            }}
+            onSubmit={
+              (values, actions) => {
+
+                fetch("/", {
+                  method: "POST",
+                  headers: { "Content-Type": "application/x-www-form-urlencoded" },
+                  body: encode({ "form-name": "cta", ...values })
+                })
+                  .then(() => {
+
+                    setMessage(true)
+                    actions.resetForm()
+
+
+                  })
+                  .catch(() => {
+
+                  })
+                  .finally(() => {
+
+                    actions.setSubmitting(false)
+
+                  })
+              }
+            }
+            validate={values => {
+              const emailRegex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i;
+              const errors = {};
+
+              if (!values.email || !emailRegex.test(values.email)) {
+                errors.email = "El email debe ser válido"
+              }
+
+              return errors;
+            }}
+          >
+            {() => (
+              <Form key="cta" name="cta" method="POST" data-netlify="true" netlify-honeypot="bot-field" className="c-cta__form">
+
+                {!message &&
+                  <>
+                    <p className="u-hidden-for-seo">
+                      <label>Don’t fill this out if you’re human: <input name="bot-field" /></label>
+                    </p>
+
+                    <input type="hidden" name="form-name" value="newsletter" />
+
+                    <Field name="email" type="email" className="c-cta__input" placeholder="Ingresá tu email..." />
+
+                    <label for="email" className="c-cta__error"><ErrorMessage name="email" /></label>
+
+                    <div data-netlify-recaptcha="true"></div>
+
+                    <button type="submit" className="c-cta__btn">Enviar</button>
+                  </>
+                }
+
+                {message && <p className="c-cta__message"><strong>Gracias por registrarte!</strong> <br /> Nos comunicaremos a la brevedad.</p>}
+
+              </Form>
+            )}
+
+          </Formik>
+
+
         </div>
       </div>
 
